4536d1319185d1154ffc724034904c8079dad05b
Matthew Evans <git@ml-evs.science>
2023-06-22 13:37:40 +0100

    Make package locally pip installable

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 import releaseVersion
 
-if __name__ == '__main__':
-    setup(name='marda_steffenBrinckmann',
-          version=0.1
-    )
+if __name__ == "__main__":
+    setup(name="marda_steffenBrinckmann", packages=find_packages(), version=0.1)
